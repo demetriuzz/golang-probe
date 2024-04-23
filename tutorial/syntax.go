3c96d6d1880362016c8dd7523a7f7546ada30d6a
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2024-04-23 20:48:36 +0500

    обновление

--- tutorial/syntax.go
+++ tutorial/syntax.go
@@ -10,13 +10,16 @@ var answer uint = 42
 
 // главная функция - точка входа
 func main() {
-	printText()
-	printDigit()
+	// вызов функций
+	printText(42)
+	printDigit(0)
+	printDigit(7)
 }
 
-func printText() {
+func printText(answer uint) {
 	// краткое объявление переменной внутри функции
 	text3 := "А я в переменной .."
+	// цикл по массиву
 	for index, value := range textArray {
 		fmt.Println(index, value)
 	}
@@ -24,13 +27,21 @@ func printText() {
 	fmt.Println(text3, answer)
 }
 
-func printDigit() {
-	for i := 0; i < 10; i++ {
+func printDigit(count uint) {
+	if count < 1 {
+		fmt.Println(count, "Счётчик меньше 1!")
+		return
+	}
+
+	// цикл с переключателем
+	for i := 0; i < int(count); i++ {
 		switch i {
 		case 0, 1, 2, 3, 4:
-			fmt.Println("меньше 5")
+			fmt.Println(i, "меньше 5")
+		case 5:
+			fmt.Println("Это пять!")
 		default:
-			fmt.Println("Больше или равно 5")
+			fmt.Println(i, "Больше чем 5")
 		}
 	}
 }
